3a5ae6d2d76df549c622ca33912ec0a5ae9fc27c
aCoderFromAnotherWorld <128140160+aCoderFromAnotherWorld@users.noreply.github.com>
2026-03-24 23:58:24 +0600

    minimarketplace

---
+++ src/main/java/com/example/minimarketplace/MinimarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.minimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimarketplaceApplication.class, args);
+	}
+
+}
